2990182de1bc9ac3bf47ec1a59996842ec4f1a44
john-jac <75442233+john-jac@users.noreply.github.com>
2022-07-28 11:02:52 -0700

    Moved requirements into own folder
    This matches what MWAA does, and also allows adding --delete into S3 sync command.  Also fixed pip3 install error.

--- docker/script/entrypoint.sh
+++ docker/script/entrypoint.sh
@@ -6,6 +6,7 @@ TRY_LOOP="20"
 : "${AIRFLOW_HOME:="/usr/local/airflow"}"
 : "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(cat /usr/local/etc/airflow_fernet_key)}}"
 : "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
+: "${REQUIREMENTS_FILE:="requirements/requirements.txt"}"
 
 # Load DAGs examples (default: Yes)
 if [[ -z "$AIRFLOW__CORE__LOAD_EXAMPLES" && "${LOAD_EX:=n}" == n ]]; then
@@ -21,22 +22,22 @@ export \
 # Install custom python package if requirements.txt is present
 install_requirements() {
     # Install custom python package if requirements.txt is present
-    if [[ -e "$AIRFLOW_HOME/dags/requirements.txt" ]]; then
+    if [[ -e "$AIRFLOW_HOME/$REQUIREMENTS_FILE" ]]; then
         echo "Installing requirements.txt"
-        pip3 install --user -r "$AIRFLOW_HOME/dags/requirements.txt"
+        pip3 install --user -r "$AIRFLOW_HOME/$REQUIREMENTS_FILE"
     fi
 }
 
 # Download custom python WHL files and package as ZIP if requirements.txt is present
 package_requirements() {
     # Download custom python WHL files and package as ZIP if requirements.txt is present
-    if [[ -e "$AIRFLOW_HOME/dags/requirements.txt" ]]; then
+    if [[ -e "$AIRFLOW_HOME/$REQUIREMENTS_FILE" ]]; then
         echo "Packaging requirements.txt into plugins"
-        pip3 download -r "$AIRFLOW_HOME/dags/requirements.txt" -d "$AIRFLOW_HOME/plugins"
+        pip3 download -r "$AIRFLOW_HOME/$REQUIREMENTS_FILE" -d "$AIRFLOW_HOME/plugins"
         cd "$AIRFLOW_HOME/plugins"
-        zip "$AIRFLOW_HOME/dags/plugins.zip" *
-        printf '%s\n%s\n' "--no-index" "$(cat $AIRFLOW_HOME/dags/requirements.txt)" > "$AIRFLOW_HOME/dags/packaged_requirements.txt"
-        printf '%s\n%s\n' "--find-links /usr/local/airflow/plugins" "$(cat $AIRFLOW_HOME/dags/packaged_requirements.txt)" > "$AIRFLOW_HOME/dags/packaged_requirements.txt"
+        zip "$AIRFLOW_HOME/requirements/plugins.zip" *
+        printf '%s\n%s\n' "--no-index" "$(cat $AIRFLOW_HOME/$REQUIREMENTS_FILE)" > "$AIRFLOW_HOME/requirements/packaged_requirements.txt"
+        printf '%s\n%s\n' "--find-links /usr/local/airflow/plugins" "$(cat $AIRFLOW_HOME/requirements/packaged_requirements.txt)" > "$AIRFLOW_HOME/requirements/packaged_requirements.txt"
     fi
 }
 
@@ -84,8 +85,8 @@ case "$1" in
     # if S3_PLUGINS_PATH
     if [ -n "$S3_PLUGINS_PATH" ]; then
       echo "Downloading $S3_PLUGINS_PATH"
-      mkdir -p /usr/local/airflow/plugins
-      cd /usr/local/airflow/plugins
+      mkdir -p $AIRFLOW_HOME/plugins
+      cd $AIRFLOW_HOME/plugins
       aws s3 cp $S3_PLUGINS_PATH plugins.zip
       unzip -o plugins.zip 
       rm plugins.zip
@@ -93,14 +94,15 @@ case "$1" in
     # if S3_DAGS_PATH
     if [ -n "$S3_DAGS_PATH" ]; then
       echo "Syncing $S3_DAGS_PATH"   
-      mkdir -p /usr/local/airflow/dags
-      cd /usr/local/airflow/dags
-      aws s3 sync --exact-timestamp $S3_DAGS_PATH .
+      mkdir -p $AIRFLOW_HOME/dags
+      cd $AIRFLOW_HOME/dags
+      aws s3 sync --exact-timestamp --delete $S3_DAGS_PATH .
     fi    
     # if S3_REQUIREMENTS_PATH
     if [ -n "$S3_REQUIREMENTS_PATH" ]; then
       echo "Downloading $S3_REQUIREMENTS_PATH"
-      aws s3 cp $S3_REQUIREMENTS_PATH /usr/local/airflow/dags/requirements.txt
+      mkdir -p $AIRFLOW_HOME/requirements
+      aws s3 cp $S3_REQUIREMENTS_PATH $AIRFLOW_HOME/$REQUIREMENTS_FILE
     fi        
     install_requirements
     airflow db init
@@ -121,7 +123,8 @@ case "$1" in
     # if S3_REQUIREMENTS_PATH
     if [ -n "$S3_REQUIREMENTS_PATH" ]; then
       echo "Downloading $S3_REQUIREMENTS_PATH"
-      aws s3 cp $S3_REQUIREMENTS_PATH /usr/local/airflow/dags/requirements.txt
+      mkdir -p $AIRFLOW_HOME/requirements
+      aws s3 cp $S3_REQUIREMENTS_PATH $AIRFLOW_HOME/$REQUIREMENTS_FILE
     fi      
     install_requirements
     ;;
@@ -129,7 +132,8 @@ case "$1" in
     # if S3_REQUIREMENTS_PATH
     if [ -n "$S3_REQUIREMENTS_PATH" ]; then
       echo "Downloading $S3_REQUIREMENTS_PATH"
-      aws s3 cp $S3_REQUIREMENTS_PATH /usr/local/airflow/dags/requirements.txt
+      mkdir -p $AIRFLOW_HOME/requirements
+      aws s3 cp $S3_REQUIREMENTS_PATH $AIRFLOW_HOME/$REQUIREMENTS_FILE
     fi      
     package_requirements
     ;;    

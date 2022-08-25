c12541d6a687ebda6f1f5c6c663051e5dfb01e57
Stéphane Soulier <stephane@soolier.com>
2022-08-25 12:33:47 +0200

    revert sync with aws repo

--- docker/script/generate_key.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Generate fernet key
-FERNET_KEY="$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")"
-
-# Store it in the image so that it can be used in entrypoint
-echo "$FERNET_KEY" > /usr/local/etc/airflow_fernet_key

4d6ff273b7e66b9e143976e6610aa10d01bb5b84
john-jac <75442233+john-jac@users.noreply.github.com>
2022-12-02 12:49:21 -0800

    Add Amazon Provider Package Fix to Local Runner
    This aligns local runner to the recent improvements in MWAA that support specifying newer Amazon provider package versions.

--- docker/script/bootstrap.sh
+++ docker/script/bootstrap.sh
@@ -1,51 +1,65 @@
 #!/bin/sh
 
 set -e
-
-# Upgrade pip version to latest
-pip3 install --upgrade pip
-
-# Install wheel to avoid legacy setup.py install
-pip3 install wheel
-
-# On RHL and Centos based linux, openssl needs to be set as Python Curl SSL library
-export PYCURL_SSL_LIBRARY=openssl
-pip3 install $PIP_OPTION --compile pycurl
-pip3 install $PIP_OPTION celery[sqs]
-
-# install postgres python client
-pip3 install $PIP_OPTION psycopg2
-
-# install minimal Airflow packages
-pip3 install $PIP_OPTION --constraint /constraints.txt apache-airflow[crypto,celery,statsd"${AIRFLOW_DEPS:+,}${AIRFLOW_DEPS}"]=="${AIRFLOW_VERSION}"
-
-# install additional python dependencies
-if [ -n "${PYTHON_DEPS}" ]; then pip3 install $PIP_OPTION "${PYTHON_DEPS}"; fi
+set -x
 
 # install adduser and add the airflow user
+yum update -y
+yum install -y shadow-utils
 adduser -s /bin/bash -d "${AIRFLOW_USER_HOME}" airflow
+yum install -y sudo
 
-# Install default providers
-pip3 install $PIP_OPTION apache-airflow-providers-amazon==${PROVIDER_AMAZON_VERSION}
+# install basic python environment and other required dependencies
+yum install -y python37 gcc gcc-g++ python3-devel python3-wheel
 
-# Install watchtower for Cloudwatch logging
-# This has to come after installing apache-airflow-providers-amazon to avoid the
-# latter overwriting the version with a previous version.
-pip3 install $PIP_OPTION watchtower==${WATCHTOWER_VERSION}
+# Upgrade pip
+sudo -u airflow pip3 install $PIP_OPTION --upgrade pip
+
+# openjdk is required for JDBC to work with Airflow
+yum install -y java-1.8.0-openjdk
+
+# install minimal Airflow packages
+echo "Installing minimal Airflow packages"
+sudo -u airflow pip3 install $PIP_OPTION --constraint /constraints.txt apache-airflow[crypto,celery,statsd"${AIRFLOW_DEPS:+,}${AIRFLOW_DEPS}"]=="${AIRFLOW_VERSION}"
+
+# install celery[sqs] and its dependencies
+yum install -y libcurl-devel openssl-devel
+# see https://stackoverflow.com/questions/49200056/pycurl-import-error-ssl-backend-mismatch
+export PYCURL_SSL_LIBRARY=openssl
+sudo -u airflow pip3 install $PIP_OPTION --compile pycurl
+sudo -u airflow pip3 install $PIP_OPTION celery[sqs]
+
+# install postgres Python driver and its dependencies
+yum install -y postgresql-devel
+sudo -u airflow pip3 install $PIP_OPTION psycopg2
+
+# install unixODBC-devel to support pyodbc
+yum install -y unixODBC-devel
+
+# install additional python dependencies
+if [ -n "${PYTHON_DEPS}" ]; then sudo -u airflow pip3 install $PIP_OPTION "${PYTHON_DEPS}"; fi
+
+echo "Installing apache-airflow-providers-amazon with version ${PROVIDER_AMAZON_VERSION}"
+sudo -u airflow pip3 install $PIP_OPTION apache-airflow-providers-amazon==${PROVIDER_AMAZON_VERSION}
+sudo -u airflow pip3 install $PIP_OPTION watchtower==${WATCHTOWER_VERSION}
 
 MWAA_BASE_PROVIDERS_FILE=/mwaa-base-providers-requirements.txt
-if [[ -f "$MWAA_BASE_PROVIDERS_FILE" ]]; then
-    echo "Installing providers supported for airflow version ${PROVIDER_AMAZON_VERSION}"
-    pip3 install $PIP_OPTION -r $MWAA_BASE_PROVIDERS_FILE
-else
-    echo "Providers not supported for airflow version ${PROVIDER_AMAZON_VERSION}"
-fi
+echo "Installing providers supported for airflow version ${PROVIDER_AMAZON_VERSION}"
+sudo -u airflow pip3 install $PIP_OPTION -r $MWAA_BASE_PROVIDERS_FILE
 
-# Use symbolic link to ensure Airflow 2.0's backport packages are in the same namespace as Airflow itself
-# see https://airflow.apache.org/docs/apache-airflow/stable/backport-providers.html#troubleshooting-installing-backport-packages
-ln -s /usr/local/airflow/.local/lib/python3.7/site-packages/airflow/providers /usr/local/lib/python3.7/site-packages/airflow/providers
+# install system dependency to enable the installation of most Airflow extras
+yum install -y mariadb-devel gcc-c++ cyrus-sasl-devel
 
-# install awscli v2, according to https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
+# jq is used to parse ECS-injected AWSSecretsManager secrets
+yum install -y jq
+
+# nc is used to check DB connectivity
+yum install -y nc
+
+# install archiving packages
+yum install -y zip unzip bzip2 gzip # tar
+
+# install awscli v2
 zip_file="awscliv2.zip"
 cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o $zip_file
@@ -59,11 +73,4 @@ cd -  # Return to previous directory
 if [ -n "$INDEX_URL" ]
 then
   pip3 freeze > /requirements.txt
-else
-  # flask-swagger depends on PyYAML that are known to be vulnerable
-  # even though Airflow 1.10 names flask-swagger as a dependency, it doesn't seem to use it.
-  if [ "$AIRFLOW_VERSION" = "1.10.12" ]
-  then
-    pip3 uninstall -y flask-swagger
-  fi
 fi

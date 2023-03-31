1c9ab0d3d9d08d3f0b7b2ff1173de2942c64e6aa
wil-teampeanut <92593003+wil-teampeanut@users.noreply.github.com>
2023-03-31 15:04:49 +0100

    Simplifying the process and amending where the information is stored will help to ensure that the information is up to date and accurate.

--- dags/custom/operators/postgres_data_to_s3.py
+++ dags/custom/operators/postgres_data_to_s3.py
@@ -1,21 +1,21 @@
 import json
 from airflow.models import BaseOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-
+from custom.hooks.postgres_query_hook import PostgresQueryHook
+from custom.functions.get_latest_rds_cpu_load import get_latest_rds_cpu_load
+import time
 
 class PostgresToS3WithSchemaOperator(BaseOperator):
 
     def __init__(
         self,
         s3_bucket: str,
-        s3_schema_dir: str,
         s3_partition_dir: str,
         s3_export_dir: str,
-        schema: str,
+        db: str,
+        db_schema: str,
         transfer_ds: str,
         table_name: str = None,
-        schema_file_suffix: str = ".json",
         postgres_conn_id: str = 'postgres_default',
         s3_conn_id: str = 'aws_default',
         *args,
@@ -25,11 +25,10 @@ class PostgresToS3WithSchemaOperator(BaseOperator):
         self.postgres_conn_id = postgres_conn_id
         self.s3_conn_id = s3_conn_id
         self.s3_bucket = s3_bucket
-        self.schema_file_suffix = schema_file_suffix
-        self.s3_schema_dir = s3_schema_dir
         self.s3_partition_dir = s3_partition_dir
         self.s3_export_dir = s3_export_dir 
-        self.schema = schema
+        self.db = db
+        self.db_schema = db_schema
         self.table_name = table_name
         self.transfer_ds = transfer_ds
 
@@ -43,87 +42,142 @@ class PostgresToS3WithSchemaOperator(BaseOperator):
 
         self.log.info(f"Exporting table {self.table_name} to S3 bucket {self.s3_bucket}")
 
-        # Get schema definition for table
-        schema_key = f"{self.s3_schema_dir}/{self.schema}/{self.table_name}{self.schema_file_suffix}"
-        schema_file = s3_hook.get_key(schema_key, self.s3_bucket).get()['Body'].read().decode('utf-8')
-        schema = json.loads(schema_file)
+        # Get the partition list for the table
+        table_key = f"{self.s3_partition_dir}/{self.db_schema}/{self.table_name}.json"
+        table_file = s3_hook.get_key(table_key, self.s3_bucket).get()['Body'].read().decode('utf-8')
+        table = json.loads(table_file)
 
-        # Get columns from schema
-        columns = schema["columns"]
-        column_names = [column["name"] for column in columns]
+        # From the partition file, get a list of partitions that have not been exported, or need to be re-exported
+        # This is a list of dictionaries
 
-        # Get time limits
-        time_column = schema['time_column']
-        where_clause = ''
-        if time_column:
-            for column in columns:
-                if column['name'] == time_column:
-                    if column['type'] == 'integer':
-                        where_clause = f"WHERE TO_TIMESTAMP({time_column})::DATE <= ''{self.transfer_ds}''::DATE"
+        for partition in table:
+            if partition['attached'] or partition['attached_on_last_export'] in [None, True]:
+                postgres_hook = PostgresQueryHook(postgres_conn_id=self.postgres_conn_id, schema=self.db)
+
+                # Delete data in dir
+                s3_prefix = f"{self.s3_export_dir}/{self.db_schema}/{self.table_name}/{partition['schema']}/{partition['name']}"
+                keys = s3_hook.list_keys(bucket_name=self.s3_bucket, prefix=s3_prefix)
+
+                # Delete all keys
+                if keys:
+                    s3_hook.delete_objects(bucket=self.s3_bucket, keys=keys)
+                    self.log.info(f"{len(keys)} objects deleted from S3 bucket {self.s3_bucket}")
+                else:
+                    self.log.info(f"No objects found with prefix {s3_prefix} in S3 bucket {self.s3_bucket}")
+
+                # Query the schema and table information
+                query = f"""
+                    SELECT
+                        table_name,
+                        column_name,
+                        data_type,
+                        column_default,
+                        is_nullable::boolean,
+                        (
+                            SELECT tc.constraint_type
+                            FROM information_schema.table_constraints tc
+                            WHERE
+                                tc.table_schema = c.table_schema AND
+                                tc.table_name = c.table_name AND
+                                tc.constraint_type = 'PRIMARY KEY' AND
+                                tc.constraint_name = c.column_name
+                            LIMIT 1
+                        ) = 'PRIMARY KEY' AS is_primary_key,
+                        (
+                            SELECT EXISTS (
+                                SELECT 1 FROM pg_indexes i
+                                WHERE
+                                    i.schemaname = c.table_schema AND
+                                    i.tablename = c.table_name AND
+                                    i.indexdef LIKE '%' || c.column_name || '%'
+                            )
+                        ) AS is_indexed
+                    FROM
+                        information_schema.columns c
+                    INNER JOIN (
+                        SELECT TABLE_NAME AS parent_table
+                        FROM information_schema.tables
+                        WHERE table_schema = 'public' AND table_type = 'BASE TABLE'
+                    ) AS t ON t.parent_table = c.table_name
+                    WHERE
+                        c.table_schema = '{partition['schema']}' AND table_name = '{partition['name']}'
+                    ORDER BY
+                        table_name, column_name
+                    """
+                rows = postgres_hook.get_results(query)
+
+                table_schema = []
+                for row in rows:
+                    table_schema.append({
+                        "name": row[1],
+                        "type": row[2],
+                        "default": row[3],
+                        "nullable": row[4],
+                        "primary_key": row[5],
+                        "index": row[6]
+                    })
+                
+                columns = [column['name'] for column in table_schema]
+
+                # find time columns
+                if self.table_name in ['message_event_tapped_at', 'message_event_received_at', 'message_event_sent_at']:
+                    time_column = 'message_event_created_at'
+                else:
+                    if 'created_at' in columns:
+                        time_column = 'created_at'
+                    elif 'timestamp' in columns:
+                        time_column = 'timestamp'
                     else:
-                        where_clause = f"WHERE {time_column}::DATE <= ''{self.transfer_ds}''::DATE"
+                        time_column = None
+                
+                schema_file = {
+                    "columns": table_schema,
+                    "time_column": time_column
+                }
+                schema_s3_key = f"{self.s3_export_dir}/{self.db_schema}/{self.table_name}/{partition['schema']}/{partition['name']}/schema.json"
+                
+                # Output the columns to a JSON file in S3
+                s3_hook.load_string(json.dumps(schema_file), key=schema_s3_key, bucket_name=self.s3_bucket, replace=True)
 
-        # Get partitions
-        partition_key = f"{self.s3_partition_dir}/{self.schema}/{self.table_name}{self.schema_file_suffix}"
-        partition_file = s3_hook.get_key(partition_key, self.s3_bucket).get()['Body'].read().decode('utf-8')
-        partitions = json.loads(partition_file)
+                # Get time limits
+                where_clause = ''
+                if time_column:
+                    for column in table_schema:
+                        if column['name'] == time_column:
+                            if column['type'] == 'integer':
+                                where_clause = f"WHERE TO_TIMESTAMP({time_column})::DATE <= ''{self.transfer_ds}''::DATE"
+                                break
+                            else:
+                                where_clause = f"WHERE {time_column}::DATE <= ''{self.transfer_ds}''::DATE"
+                                break
+                
+                # Construct query
+                # Add column name in quotes because one table has a column called "do" (in quotes) and another table has a column called reference, which is a reserved word in sql
+                select_clause = ", ".join([ f'"{column}"' for column in columns])
+                table_query = f"SELECT {select_clause} FROM {partition['path']} {where_clause}"
+                
+                # Export data to S3
+                data_s3_key = f"{self.s3_export_dir}/{self.db_schema}/{self.table_name}/{partition['schema']}/{partition['name']}/data.csv"
 
-        previously_exported_partitions = [f"{partition['source_schema']}.{partition['partition_name']}" for partition in partitions['previously_exported_partitions']]
-        active_partitions = [f"{partition['source_schema']}.{partition['partition_name']}" for partition in partitions['active_partitions']]
-        previously_active_partitions = [f"{partition['source_schema']}.{partition['partition_name']}" for partition in partitions['previously_active_partitions']]
-        inactive_partitions = [f"{partition['source_schema']}.{partition['partition_name']}" for partition in partitions['inactive_partitions']]
+                # Using the above query in the export function
+                export_query = f"SELECT * FROM aws_s3.query_export_to_s3('{table_query}', aws_commons.create_s3_uri('{self.s3_bucket}', '{data_s3_key}', 'us-east-1'), options :='format csv');"
+                
+                while get_latest_rds_cpu_load('peanut-prod-replica1', 'aws_default') > 75:
+                    self.log.info(f"RDS CPU load is high. Waiting 20 seconds before exporting data")
+                    time.sleep(20)
+                postgres_hook.run(export_query)
 
-        partitions_to_process = active_partitions
-
-        for partition in previously_active_partitions:
-            if partition not in partitions_to_process:
-                partitions_to_process.append(partition)
-        
-        for partition in inactive_partitions:
-            if partition not in partitions_to_process and partition not in previously_exported_partitions:
-                partitions_to_process.append(partition)
-        
-        for partition in partitions_to_process:
-            # Example partition name: public.users or partitions.user_session_20231212
-            postgres_hook = PostgresHook(postgres_conn_id=self.postgres_conn_id, connect_timeout=600)
-
-            # Delete data in dir
-            s3_prefix = f"{self.s3_export_dir}/{self.schema}/{self.table_name}/{partition.split('.')[0]}/{partition.split('.')[1]}"
-            keys = s3_hook.list_keys(bucket_name=self.s3_bucket, prefix=s3_prefix)
-
-            # Delete all keys
-            if keys:
-                s3_hook.delete_objects(bucket=self.s3_bucket, keys=keys)
-                self.log.info(f"{len(keys)} objects deleted from S3 bucket {self.s3_bucket}")
-            else:
-                self.log.info(f"No objects found with prefix {s3_prefix} in S3 bucket {self.s3_bucket}")
-            
-            # Construct query
-            select_clause = ", ".join([ '"' + column_name + '"' for column_name in column_names])
-            query = f"SELECT {select_clause} FROM {partition} {where_clause}"
-            
-            # Export data to S3
-            s3_key = f"{self.s3_export_dir}/{self.schema}/{self.table_name}/{partition.split('.')[0]}/{partition.split('.')[1]}/{partition.split('.')[1]}.csv"
-
-            sql = f"SELECT * FROM aws_s3.query_export_to_s3('{query}', aws_commons.create_s3_uri('{self.s3_bucket}', '{s3_key}', 'us-east-1'), options :='format csv');"
-            postgres_hook.run(sql)
-            
-            if partition not in previously_exported_partitions:
-                partitions['previously_exported_partitions'].append(
-                    {
-                        'source_schema': partition.split(".")[0],
-                        'partition_name': partition.split(".")[1],
-                        'updated_ds': self.transfer_ds
-                        }
+                # Update partition information
+                partition['last_exported_ds'] = self.transfer_ds
+                partition['attached_on_last_export'] = partition['attached']
+                            
+                file_contents = json.dumps(table, indent=4)
+                s3_hook.load_string(
+                    string_data=file_contents,
+                    key=table_key,
+                    bucket_name=self.s3_bucket,
+                    replace=True
                 )
-            
-            file_contents = json.dumps(partitions, indent=4)
-            s3_hook.load_string(
-                string_data=file_contents,
-                key=partition_key,
-                bucket_name=self.s3_bucket,
-                replace=True
-            )
-            
-            self.log.info(f"Outputted partition information for table {partition} to S3 key {s3_key}")
+                
+                self.log.info(f"Outputted partition information for table {partition}")
             
--- dags/etl_data_transfer.py
+++ dags/etl_data_transfer.py
@@ -8,10 +8,10 @@ from airflow.utils.weight_rule import WeightRule
 from custom.operators.store_ds_macro_operator import StoreDsMacroOperator
 from airflow.models import Variable
 
-from custom.operators.postgres_schema_to_s3 import PostgresSchemaToS3Operator
 from custom.operators.postgres_partitions_to_s3 import PostgresPartitionsToS3Operator
-from custom.operators.verify_s3_and_postgres import VerifyS3AndPostgresOperator
 from custom.operators.postgres_data_to_s3 import PostgresToS3WithSchemaOperator
+# import dummy_operator
+from airflow.operators.dummy_operator import DummyOperator
 
 S3_BUCKET = Variable.get('etl_s3_bucket', deserialize_json=False)
 POSTGRES_DB = Variable.get('etl_postgres_db', deserialize_json=False)
@@ -51,18 +51,6 @@ with DAG(
     # retrieve etl_data_transfer_ds variable and 
     etl_data_transfer_ds = Variable.get('etl_data_transfer_ds', deserialize_json=False, default_var=None)
 
-    # Transfer the ETL schema to S3
-    postgres_schema_to_s3 = PostgresSchemaToS3Operator(
-        task_id='postgres_schema_to_s3',
-        postgres_conn_id='postgres_default',
-        postgres_db=POSTGRES_DB,
-        s3_conn_id='aws_default',
-        s3_bucket=S3_BUCKET,
-        schema='public',
-        transfer_ds = etl_data_transfer_ds,
-        dag=dag
-    )
-
     # Transfer the table partitions to S3
     postgres_partitions_to_s3 = PostgresPartitionsToS3Operator(
         task_id='postgres_partitions_to_s3',
@@ -70,42 +58,36 @@ with DAG(
         postgres_db=POSTGRES_DB,
         s3_conn_id='aws_default',
         s3_bucket=S3_BUCKET,
+        s3_prefix='table_list',
         schema='public',
         transfer_ds = etl_data_transfer_ds,
+        table_list_variable_name='peanut_analytics_tables',
         dag=dag
     )
 
-    verify_metadata = VerifyS3AndPostgresOperator(
-        task_id='verify_metadata',
-        postgres_db = POSTGRES_DB,
-        s3_bucket = S3_BUCKET,
-        # TODO: Should we be defining the below directories glabally and passing them to the tasks above?
-        s3_schema_dir = 'metadata/schema',
-        s3_partition_dir = 'metadata/partitions',
-        table_list_variable_name = 'peanut_analytics_tables',
+    start_table_processing = DummyOperator(
+        task_id='start_table_processing',
         dag=dag
     )
 
     peanut_analytics_tables = Variable.get('peanut_analytics_tables', deserialize_json=True, default_var=None)
 
-    for table in peanut_analytics_tables:
+    for table in peanut_analytics_tables[0:10]:
         postgres_data_to_s3 = PostgresToS3WithSchemaOperator(
             task_id = f"{table}_to_s3",
             s3_bucket = S3_BUCKET,
-            s3_schema_dir = 'metadata/schema',
-            s3_partition_dir = 'metadata/partitions',
+            s3_partition_dir = 'table_list',
             s3_export_dir = 'export',
-            schema = 'public',
+            db=POSTGRES_DB,
+            db_schema = 'public',
             table_name = table,
             transfer_ds = etl_data_transfer_ds
         )
-        verify_metadata >> postgres_data_to_s3
+        start_table_processing >> postgres_data_to_s3
 
 
     store_ds_macro >> [
-        postgres_schema_to_s3,
         postgres_partitions_to_s3
-    ] >> verify_metadata
-
+    ] >> start_table_processing
 
 # add the original partition name to the output of the dump
